5342b498d692c4447cc8b9d42626e593b97b1a46
ajai <ajai.rvt@gmail.com>
2021-01-16 11:59:54 +0530

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component'
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent },
+  { path: 'login', component: LoginComponent },
+  // { path: 'contact', component: HeaderComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
